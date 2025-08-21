97c11b208db01487894b41dc37a0df4a2d4dffc4
ullahhab <hammadullahris@gmail.com>
2025-08-20 22:06:32 -0500

    initial react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Welcome to my portfolio and Project showcase</h1>
+      <p>This is a starter React site and this will have more components. This is to showcase I have react skills</p>
+    </div>
+  );
+}
+
+export default App;
