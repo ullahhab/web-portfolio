e570aa0ef7aeeff301c3451a49159ed0f458bc5b
ullahhab <hammadullahris@gmail.com>
2025-08-20 22:35:43 -0500

    update to projects page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Welcome to my portfolio and Project showcase</h1>
-      <p>This is a starter React site and this will have more components. This is to showcase I have react skills</p>
+      <p>This is a starter React site and this will have more components. This is to showcase I have react skills t</p>
       <Projects/>
     </div>
   );
