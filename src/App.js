d9179452480852fef0979f5fa134e7ba9b4aa783
ullahhab <hammadullahris@gmail.com>
2025-08-20 22:33:12 -0500

    update to projects page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Projects from './projects';
 
 function App() {
   return (
     <div className='App'>
       <h1>Welcome to my portfolio and Project showcase</h1>
       <p>This is a starter React site and this will have more components. This is to showcase I have react skills</p>
+      <Projects/>
     </div>
   );
 }
---
+++ src/projects.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Project = [
+    {
+        title: "Stock Bot",
+        description: "Build a trading bot using Alpaca API. You can download a docker container from the link",
+        link: "https://github.com/ullahhab"
+    },
+    {
+        title: "Recipe Manager",
+        description: "Save, build, and rotate recipe for the weeks",
+        link: "https://github.com/ullahhab"
+    },
+    {
+        title: "Automations",
+        description: "API building automation, AWS dashboard builder all in one, trading bot, Whitelisting, Save commands to run your projects with one click and many more",
+        link: "https://github.com/ullahhab"
+    }
+];
+export default function Projects() {
+    return(
+        <div style={{ padding: "2rem"}}>
+            <h2>My Projects</h2>
+            <div style={{display: "grid", gap: "1.5rem", gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))"}}>
+                {Projects.map((project, index) => {
+                    <div
+                    key={index}
+                    style={{
+                        padding: "1rem",
+                        border: "1px solid #ddd",
+                        borderRadius: "10px",
+                        boxShadow: "0 4px 8px rgba(0,0,0,0.1)"
+                    }}>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            🔗 View on GitHub
+                        </a>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+}
